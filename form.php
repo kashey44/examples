62dd94f836d07cbd1a148d2a92ca0bda7952e289
kashey44 <kashey1@gmail.com>
2015-06-25 21:26:49 +0300

    added files
    bootstrap files

---
+++ form.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/libs/bootstrap/css/bootstrap.min.css"/>
+</head>
+<body>
+    <style>
+    
+    </style>
+    <form>
+        <label for="username">Login</label><input type="text" id="username" name="username" size="8" /><br/>
+        <label for="password">Password</label><input type="text" id="password" name="password"  size="8" /><br/>
+        <label for="captcha">Captcha</label><input type="text" id="captcha" name="captcha"  size="8" /><br/>
+        <br/>
+        <input type="submit" name="send" value="send"/>
+    
+    </form>
+</body>
+</html>
+<?php
+
+?>
